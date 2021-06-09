b85296c4c227dc20d0910d1848f3cf3d14e63755
Denis K <tokcdk@gmail.com>
2021-06-09 19:08:27 +0500

    fixed incorrect group title insertion

--- iptvlistsmerger/Form1.cs
+++ iptvlistsmerger/Form1.cs
@@ -249,7 +249,7 @@ namespace iptvlistsmerger
         private string SetGroupTitle(string value, string lastGroup, out string groupName, bool EXTGRP = false)
         {
             var parts = value.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-            var reg = Regex.Match(parts[0], @"#EXTINF(:)");
+            var reg = Regex.Match(parts[0], @"#EXTINF(:-?[0-9]{0,12})");
             groupName = (EXTGRP || lastGroup.Length > 0 ? lastGroup : "Разное");
             parts[0] = parts[0].Insert(parts[0].IndexOf(reg.Result("$1")) + reg.Result("$1").Length, " group-title=\"" + groupName + "\"");
 
